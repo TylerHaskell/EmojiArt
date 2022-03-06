9b49460d90c8a14a76898b2b3a43be63ef48b1bb
Tyler Haskell <tyler.haskell@concur.com>
2022-03-05 18:55:07 -0600

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Haskell, Tyler on 3/5/22.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
